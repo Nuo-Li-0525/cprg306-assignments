eeb56282990674af8064ae02f9e18943237a3269
Nuo-Li-0525 <134658624+Nuo-Li-0525@users.noreply.github.com>
2024-09-19 10:57:07 -0600

    Updated Week 2 assignment components
    Added headers and navigation links to app/page.js.
    Created week-2 page with "Shopping List" and added student-info.js for GitHub link display.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <p>CPRG 306: Web Development 2 - Assignments</p>
+      <Link href="./week-2/">Week 2 Assignment</Link>
+    </main>
+  );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Nuo Li</p>
+      <Link href="https://github.com/Nuo-Li-0525/cprg306-assignments">
+        https://github.com/Nuo-Li-0525/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
